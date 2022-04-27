9c7a6a579c0a6b282a46cb20279b1374f68c5668
NitheshBonugu <77087049+NitheshBonugu@users.noreply.github.com>
2022-04-27 02:09:33 -0500

    Update game2.py

--- game2.py
+++ game2.py
@@ -1,5 +1,6 @@
 import csv
 import operator
+from more_itertools import first
 import pandas as pd
 
 data = pd.read_csv("gameData.csv")
@@ -20,5 +21,40 @@ print("========================================================")
 bottomFive = data.tail(5)
 print(bottomFive)
 print('\n')
+
+
+print("=====================================================================")
+print("How many players who scored more than the average for each game:     ")
+print("=====================================================================")
+average = data['Points'].mean()
+
+gameOnePlayer, gameTwoPlayer, gameThreePlayer, gameFourPlayer, gameFivePlayer = 0, 0, 0, 0, 0
+
+for i in range(len(data['Points'])):
+
+    firstColumn = data["Game"]
+    secondColumn = data["Points"]
+
+    if firstColumn[i] == 1 and secondColumn[i] >= average:
+        gameOnePlayer += 1
+    if firstColumn[i] == 2 and secondColumn[i] >= average:
+        gameTwoPlayer += 1
+    if firstColumn[i] == 3 and secondColumn[i] >= average:
+        gameThreePlayer += 1
+    if firstColumn[i] == 4 and secondColumn[i] >= average:
+        gameFourPlayer += 1
+    if firstColumn[i] == 5 and secondColumn[i] >= average:
+        gameFivePlayer += 1
+
+    for row in range(5):
+        if firstColumn[i] == 1 and secondColumn[i] >= average:
+            data[data['Points']]
+
+print("Players from game one who scored above the average: ", gameOnePlayer)
+print("Players from game two who scored above the average: ", gameTwoPlayer)
+print("Players from game three who scored above the average: ", gameThreePlayer)
+print("Players from game four who scored above the average: ", gameFourPlayer)
+print("Players from game five who scored above the average: ", gameFivePlayer)
+
 
 
